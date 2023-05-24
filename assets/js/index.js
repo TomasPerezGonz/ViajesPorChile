ae45e86642788287a5002de04f79aa7a0b69fe4b
Tomas Perez <tomasperezg@gmail.com>
2023-05-24 13:33:07 -0400

    Alert de mensaje enviado OK

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,3 +2,10 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
+/*Alert envio mensaje*/
+const formulario = document.querySelector('#formulario')
+formulario.addEventListener('submit', event => {
+    event.preventDefault()
+    alert('Tu mensaje ha sido enviado correctamente')
+})
+
