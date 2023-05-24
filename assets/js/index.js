c38e54c83a800dc74c0f47d4a2f51632d3093001
Tomas Perez <tomasperezg@gmail.com>
2023-05-23 21:06:22 -0400

    Submit ok

---
+++ assets/js/index.js
@@ -0,0 +1,4 @@
+/* Implementación Tooltips */
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
